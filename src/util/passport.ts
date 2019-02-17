ebc0ca8172a465f765e6aaebf4bc5c41bf3207fc
Juan de Urtubey <jdeurt@gmail.com>
2019-02-16 18:13:28 -0600

    Added serialization.

--- src/util/passport.ts
+++ src/util/passport.ts
@@ -2,6 +2,24 @@ import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import { Patient, Doctor, HealthcareProvider, PatientInterface, DoctorInterface, HProviderInterface } from "../models";
 
+passport.serializeUser((user: PatientInterface | DoctorInterface | HProviderInterface, done) => {
+    done(null, user.id);
+});
+  
+passport.deserializeUser((id, done) => {
+    Patient.findById(id, (err, user) => {
+        if (!err) done(err, user as object);
+
+        Doctor.findById(id, (err, user) => {
+            if (!err) done(err, user as object);
+
+            HealthcareProvider.findById(id, (err, user) => {
+                done(err, user as object);
+            });
+        })
+    });
+});
+
 passport.use("patient", new LocalStrategy({
     usernameField: "email"
 }, (username, password, done) => {
